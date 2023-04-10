62845b3ea2a0da3a6dc01359ad6e3a3cc4a1114d
Aaqib Parvez <aaqib29292@gmail.com>
2023-04-10 14:43:39 +0530

    added vercel json and changed post to get

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import createTemplate from "./create-template";
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = 8000;
 
-app.post("/", async (req, res) => {
+app.get("/", async (req, res) => {
     // Calling the template render func with dynamic data
     const result = await createTemplate(req.body);
-  
+
     // Setting up the response headers
     res.setHeader("Content-Type", "application/pdf");
     res.setHeader("Content-Disposition", `attachment; filename=export.pdf`);
-  
+
     // Streaming our resulting pdf back to the user
     result.pipe(res);
 });
